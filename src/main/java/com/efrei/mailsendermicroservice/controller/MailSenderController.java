0ed4b0044e561bde526722cbed447b6be5e4c642
julesV <julesvanin@gmail.com>
2024-02-29 16:07:28 +0100

    update : changed the endpoint's path

--- src/main/java/com/efrei/mailsendermicroservice/controller/MailSenderController.java
+++ src/main/java/com/efrei/mailsendermicroservice/controller/MailSenderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/mail")
+@RequestMapping("/mail-sender/send")
 public class MailSenderController {
     private final MailSenderService emailingService;
 
